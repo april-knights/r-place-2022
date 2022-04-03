ee45f03fac26fecc2699cde09f1f13638f936301
LeoVerto <LeoVerto@users.noreply.github.com>
2022-04-03 13:27:50 +0200

    Use April Knights overlay

--- overlay.user.js
+++ overlay.user.js
@@ -12,7 +12,7 @@ if (window.top !== window.self) {
     window.addEventListener('load', () => {
         // Load the image
         const image = document.createElement("img");
-        image.src = "https://cdn.mirai.gg/tmp/dotted-place-template.png";
+        image.src = "https://april-knights.github.io/pixel/overlay.png";
         image.onload = () => {
             image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
         };
